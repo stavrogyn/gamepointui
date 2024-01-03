95ddf66595ce12295b5f110a2d1ce1b871163216
Nikita Krugovoy <nikitakrugovoy@gmail.com>
2024-01-04 00:12:35 +0100

    feat(reservation): implement cards for diff rooms

--- screens/reservation/login.tsx
+++ screens/reservation/login.tsx
@@ -1,16 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, useCallback, useState } from 'react';
 import { View, Text, Card, Button, TextField } from 'react-native-ui-lib';
 
 import { styles } from './styles';
 
-export class ReservationScreen extends Component {
-  render() {
+export const ReservationScreen = () => {
+  const [isExtended, setIsExtended] = useState(false);
+
+  const handleCardClick = useCallback(() => setIsExtended(true), [setIsExtended]);
+
+  const handleGoBackClick = useCallback(() => setIsExtended(false), [setIsExtended]);
+
+  if (isExtended) {
     return (
       <View flex padding-page style={{ flexDirection: 'column', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: 30 }}>
-         <Card height={100} center padding-s4 marginB-s4>
-          <Text body>Some reservation screen staff</Text>
+        <Card height={100} center padding-s4 marginB-s4 style={{ width: '100%', }}>
+          <Text body style={{ color: '#7F35B6' }}>SOME PCS MAP</Text>
+        </Card>
+        <Card height={100} center padding-s4 marginB-s4 style={{ width: '100%', }} onTouchEnd={handleGoBackClick}>
+          <Text body style={{ color: '#7F35B6' }}>Go back</Text>
         </Card>
       </View>
-    );
+    )
   }
+
+  return (
+    <View flex padding-page style={{ flexDirection: 'column', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: 30 }}>
+       <Card height={100} center padding-s4 marginB-s4 style={{ width: '100%' }} onTouchEnd={handleCardClick}>
+        <Text body style={{ color: '#7F35B6' }}>Regular PC Room</Text>
+       </Card>
+       <Card height={100} center padding-s4 marginB-s4 style={{ width: '100%', }} onTouchEnd={handleCardClick}>
+        <Text body style={{ color: '#7F35B6' }}>Pro PC Room</Text>
+       </Card>
+    </View>
+  );
 }
